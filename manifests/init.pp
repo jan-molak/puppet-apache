07e95671dca6b1dffffa7f9c3ae8614c17e62421
jan-molak <jan.molak@gmail.com>
2013-01-11 10:41:26 +0000

    Module can now apply config files if they're found under /vagrant/files/apache or within the module itself

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,9 @@
 class apache (
   $with_mods = []
 ) {
+  stage { 'apache2_post': require => Stage['main'] }
+  class { 'apache2::post': stage => 'apache2_post'; }
+
   package { 'apache2':  ensure => present }
 
   group   { 'www-data': ensure => present }
@@ -32,6 +35,45 @@ class apache (
     exec { "a2enmod ${name}":
         require => Package["apache2"],
         notify  => Exec["force-reload-apache2"],
+    }
+  }
+}
+
+class apache2::post {
+  $confd_source = only_existing_sources([
+    '/vagrant/files/apache/etc/apache2/conf.d',
+    'puppet:///modules/apache/etc/apache2/conf.d'
+  ])
+
+  if $confd_source != [] {
+    file { "/etc/apache2/conf.d":
+        ensure  => directory,
+        recurse => true,
+        force   => true,
+        owner   => "root",
+        group   => "root",
+        mode    => 0644,
+        source  => $confd_source,
+        notify  => Exec["force-reload-apache2"],
+    }
+  }
+
+  $sites_enabled_source = only_existing_sources([
+    '/vagrant/files/apache/etc/apache2/sites-enabled',
+    'puppet:///modules/apache/etc/apache2/sites-enabled'
+  ])
+
+  if $sites_enabled_source != [] {
+    file { "/etc/apache2/sites-enabled":
+      ensure  => directory,
+      recurse => true,
+      purge   => true,
+      force   => true,
+      owner   => "root",
+      group   => "root",
+      mode    => 0644,
+      source  => $sites_enabled_source,
+      notify  => Exec["force-reload-apache2"],
     }
   }
 
